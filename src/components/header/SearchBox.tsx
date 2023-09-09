a2dbba82591188309006aaf23c482268f2cb1b8d
Adnan Ali <adnanali25753@gmail.com>
2023-09-10 01:01:57 +0500

    replaced favicon

--- src/components/header/SearchBox.tsx
+++ src/components/header/SearchBox.tsx
@@ -3,16 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const SearchBox = () => {
   return (
     <>
-      <div className="text-[#606266] h-full relative flex items-center">
-        <FontAwesomeIcon
-          icon="magnifying-glass"
-          className="absolute left-3 z-50"
-        />
-        <input
-          type="search"
-          placeholder="Search Uni Chat"
-          className="text-sm outline-none ps-9 pe-3 focus:scale-105 bg-gray-100 w-[200px] h-full rounded-full  px-3outline-none transition-all duration-200"
-        />
+      <div className="flex items-center h-full">
+        <img src="../../assets/images/Uni_Chat_logo.png" className="w-[40px]" />
+        <div className="text-[#606266] h-full relative flex items-center">
+          <FontAwesomeIcon
+            icon="magnifying-glass"
+            className="absolute left-3 z-50"
+          />
+          <input
+            type="search"
+            placeholder="Search Uni Chat"
+            className="text-sm outline-none ps-9 pe-3 focus:scale-105 bg-gray-100 w-[200px] h-full rounded-full  px-3outline-none transition-all duration-200"
+          />
+        </div>
       </div>
     </>
   );
