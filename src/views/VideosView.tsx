f0d8e6933f2599c1da915b93b072aaaa63ce6229
Adnan Ali <adnanali25753@gmail.com>
2023-10-06 15:04:51 +0500

    fixed some issue in videos view page

--- src/views/VideosView.tsx
+++ src/views/VideosView.tsx
@@ -1,9 +1,7 @@
-import ImageSlider from "../components/ImageSlider/ImageSlider";
-
 const VideosView = () => {
   return (
-    <div>
-      <ImageSlider />
+    <div className="flex items-center justify-center p-11 text-xl font-bold text-text-gray">
+      Videos
     </div>
   );
 };
