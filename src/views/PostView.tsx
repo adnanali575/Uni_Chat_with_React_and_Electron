a33f56cdc0c6851af931a0e12bc0696249bd5448
Adnan Ali <adnanali25753@gmail.com>
2023-10-06 17:50:59 +0500

    updated postView apge

--- src/views/PostView.tsx
+++ src/views/PostView.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { getDoc, db, doc } from "../firebase/firebaseConfig";
 import ImageSlider from "../components/ImageSlider/ImageSlider";
 import { PostType } from "../types";
+import { dateFormater, timeFormater } from "../helpers";
+import PostDescription from "../components/postCard/PostDescription";
 
 const PostView = () => {
   const [postData, setpostData] = useState<PostType>();
@@ -26,7 +28,26 @@ const PostView = () => {
           {postData?.postFiles && <ImageSlider files={postData.postFiles} />}
         </div>
       </div>
-      <div className="sm:w-full bg-blue md:w-[400px] lg:w-[500px]">Hello</div>
+      <div className="sm:w-full md:w-[400px] lg:w-[500px]">
+        {/* -------------------------------------------------------------- */}
+        <div className="bg-white h-full shadow-md p-2">
+          <div className="flex cursor-pointer">
+            <div className="w-[50px] h-[50px] rounded-full overflow-hidden flex items-center justify-center">
+              <img className="w-full h-fit" src={postData?.ownerProfile} />
+            </div>
+            <div className="px-2">
+              <p className="font-bold">{postData?.ownerName}</p>
+              {postData?.publishDate && (
+                <p className="text-xs">
+                  {dateFormater(postData.publishDate)}
+                  {timeFormater(postData.publishDate)}
+                </p>
+              )}
+            </div>
+          </div>
+          {postData?.description && <PostDescription post={postData} />}
+        </div>
+      </div>
     </div>
   );
 };
